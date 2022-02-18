f0f0054ac1f7a1f574df80a578a115fad6a41c5d
Donghyeon5 <99226677+Donghyeon5@users.noreply.github.com>
2022-02-18 12:13:23 +0900

    Delete script directory

--- script/popup.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".menu1, .menu2, .menu3").click(function(){
-        $(".popup").show();//.fadeIn(); 서서히 나타남.
-    });
-    $(".popup a").click(function(){
-        $(".popup").hide();//.fadeOut(); 서서히 사라짐.
-    });
-});
